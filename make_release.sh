ce1705b5918cb1e43e40f2a60bf5458241777cba
mckib2 <nicholas.bgp@gmail.com>
2019-12-09 13:23:33 -0700

    remove user option from pip commands for venv compatitibility

--- make_release.sh
+++ make_release.sh
@@ -3,9 +3,9 @@
 rm -rf dist
 
 # Generate distribution archives
-python -m pip install --user --upgrade setuptools wheel
+python -m pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 
 # Upload
-python -m pip install --user --upgrade twine
+python -m pip install --upgrade twine
 python -m twine upload dist/*
