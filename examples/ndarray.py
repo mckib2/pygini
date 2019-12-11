4a3935797df099d68932dd3c673ce1d4aac35b30
Nicholas <nicholas.bgp@gmail.com>
2019-12-10 20:05:54 -0700

    Use better example testing ndarrays

--- examples/ndarray.py
+++ examples/ndarray.py
@@ -10,14 +10,7 @@ if __name__ == '__main__':
     N = 100
     A = RG.random((N, N-20, N-20))
 
-    # Control: manually along 0th axis
-    G0 = np.zeros(A.shape[1:])
-    for ii in range(A.shape[1]):
-        for jj in range(A.shape[2]):
-            G0[ii, jj] = gini(A[:, ii, jj])
-
-    # Compute Gini along 0th axis
+    # Compare results:
+    G0 = np.apply_along_axis(gini, axis=0, arr=A)
     G = gini(A, axis=0)
-
-    # Sanity check:
     assert np.allclose(G0, G), 'pygini is not working correctly!'
